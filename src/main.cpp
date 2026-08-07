c9e6c2695c2d1bd18f5dcee5f8199c1f80b09441
Ben Cartwright <bencartwright@live.com.au>
2026-08-07 20:51:33 +1000

    commiting missed files from last commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,26 @@
 #include "Crew.h"
 #include "Savegame.h"
 #include "Loadgame.h"
+#include "Charactersheet.h"
 
 
 int main() {
-    std::cout << "Hello World!\n";
-    Crew myprinter("Captain");
-    Save mysaver("filler?");
-    Load myloader("filler?");
+    Crew myprinter("");
+    Save mysaver("");
+    Load myloader("");
+    Char_sheet print_sheet("");
+    int mynum1;
 
-
+    //myprinter.printMessage();
     myloader.load_game();
-    //display loaded data here
+    std::cout << "Which Character would you like to view:\nChar 1: press 1\n";
+    std::cin >> mynum1;
+    if (mynum1 == 1){
+        print_sheet.printMessage();//display loaded data here
+    } else {
+        std::cout << "thanks for playing\n";
+    }
+    
     mysaver.save_game();
-    myprinter.printMessage();
     return 0;
 }

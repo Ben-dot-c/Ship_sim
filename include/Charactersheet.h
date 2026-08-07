bb95d07cf7306cd0607e3b522a1cd0ce3a3e096e
Ben Cartwright <bencartwright@live.com.au>
2026-08-07 20:47:04 +1000

    prompts for user input and cleaner printing

---
+++ include/Charactersheet.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+class Char_sheet {
+public:
+    Char_sheet(std::string);
+    void printMessage();
+};
---
+++ src/Charactersheet.cpp
@@ -0,0 +1,24 @@
+#include "Charactersheet.h"
+#include "Loadgame.h"
+#include <cstdio>
+
+Char_sheet::Char_sheet(std::string) {
+
+}
+void Char_sheet::printMessage() {
+    Load loader("filler");
+    Load::stat_holder result = loader.load_game();
+
+    int xp = 67;
+
+    std::printf ( "+-------------------+\n"
+                  "|CHAR NAME|  %d/100 |\n"
+                  "|---------|---------|\n"
+                  "|STR      |   %d    |\n"
+                  "|INT      |   %d    |\n"
+                  "|WEAP     |   %d    |\n"
+                  "|REPAIR   |   %d    |\n"
+                  "|MEDIC    |   %d    |\n"
+                  "+-------------------+\n",
+                 xp, result.str, result.intel, result.weap, result.rep, result.med);
+} 
